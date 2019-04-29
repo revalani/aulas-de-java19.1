fbdb3096a998749da24cdf4094b9962e742f07a8
revalani <renanvalani@gmail.com>
2019-04-29 09:58:12 -0300

    implementação de TDD: Album (dois testes falam)
    Test-Driven Development (TDD)
    
    Estão falhando os testes:
      getQuantasFaltamTest();
      getContFigurinhasTest()

---
+++ Album/src/Album.java
@@ -0,0 +1,54 @@
+public class Album {
+
+    private int tamanhoAlbum;  // total de figurinhas do álbum completo
+    private int itensColados[];
+
+    public Album(String nome, int tamanhoAlbum) {
+        this.tamanhoAlbum = tamanhoAlbum;
+        this.itensColados = new int[tamanhoAlbum+1];
+    }
+
+    public boolean possuiFigurinha(int posicao) {
+        if (!(posicao < this.tamanhoAlbum && posicao > 0)){
+            return false;
+        }
+        else{
+            return this.itensColados[posicao] > 0;
+        }
+    }
+
+    public boolean possuiRepetida(int posicao) {
+        if (!(posicao < this.tamanhoAlbum && posicao > 0)){
+            return false;
+        }
+        else{
+            return this.itensColados[posicao] >1;
+        }
+    }
+
+    public void colarFigura(Figurinha figura){
+        this.itensColados[figura.numeroDaFigurinha] +=1;
+    }
+
+    public void abrirPacote(Figurinha[] pacote) {
+        for (int i = 0; i < pacote.length; i++) {
+            colarFigura(pacote[i]);
+        }
+    }
+
+    public int getTamanhoAlbum() {
+        return this.tamanhoAlbum;
+    }
+
+    /**
+     * responde a qtds de figurinhas coladas no album
+     * @return quantidade de figurinhas que estão coladas no album
+     */
+    public int getContFigurinhas() {
+        return this.itensColados.length-1;
+    }
+
+    public int getQuantasFaltam() {
+        return this.tamanhoAlbum-this.getContFigurinhas();
+    }
+}
---
+++ Album/src/Figurinha.java
@@ -0,0 +1,8 @@
+public class Figurinha {
+
+    int numeroDaFigurinha;
+
+    public Figurinha(int posicao) {
+        this.numeroDaFigurinha = posicao;
+    }
+}
